f4d24a29cdce37c667084277dec75e0ff8c6bc6f
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2022-08-20 16:47:12 +0200

    Update discord4j-core to 3.2.3 (#34)

--- project/build.scala
+++ project/build.scala
@@ -14,7 +14,7 @@ object Dep {
     "dev.zio" %% "zio-interop-reactivestreams" % "2.0.0-RC7"
 //val zioInteropCats = "dev.zio" %% "zio-interop-cats" % "3.1.1.0"
 
-  val discord4j = "com.discord4j" % "discord4j-core" % "3.2.2"
+  val discord4j = "com.discord4j" % "discord4j-core" % "3.2.3"
   val reactiveStreams = "org.reactivestreams" % "reactive-streams" % "1.0.4"
 
   // For some reason this isn't picked up by the dependency resolver
